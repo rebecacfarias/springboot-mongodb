80d0ea297c66a4d5738b438e0006c4d4c5870063
Rebeca Farias <fariasrebecac@gmail.com>
2021-03-23 18:01:53 -0300

    adicionando comentários

--- src/main/java/com/training/mongospring/config/Instantiation.java
+++ src/main/java/com/training/mongospring/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.training.mongospring.domain.Post;
 import com.training.mongospring.domain.User;
 import com.training.mongospring.dto.AuthorDTO;
+import com.training.mongospring.dto.CommentDTO;
 import com.training.mongospring.repository.PostRepository;
 import com.training.mongospring.repository.UserRepository;
 
@@ -44,11 +45,19 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/12/2012"), "Partiu viagem", "Vou viajar pra Marte!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/12/2012"), "To na nave", "O wi fi chega aqui na nave gente!",new AuthorDTO(maria));
 
+		CommentDTO comment1 = new CommentDTO("Boa viagem amiga!", sdf.parse("21/12/2012"), new AuthorDTO(alex));
+		CommentDTO comment2 = new CommentDTO("Chocado!", sdf.parse("21/12/2012"), new AuthorDTO(bob));
+		
+		post1.getComments().add(comment1);
+		post2.getComments().add(comment2);
+		
 		postRepository.save(post1);
 		postRepository.save(post2);
 		
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
 		userRepository.save(maria);
+		
+		
 	}
 	
 }
